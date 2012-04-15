06218a6b93fbfec167958b30eb7271d6ec4d974f
dourgulf <ldw@g4next.com>
2012-04-15 16:52:14 +0800

    Initial Commit

---
+++ gamenpc/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  gamenpc
+//
+//  Created by Dawen Rie on 12-4-15.
+//  Copyright (c) 2012年 G4 Workshop. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
